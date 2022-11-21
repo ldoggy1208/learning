40eccb675a1607a9e76e74055d066ef7642d8e95
Liam Temple <75604231+ldoggy1208@users.noreply.github.com>
2022-11-21 08:48:41 -0500

    Update combat_sim_multiplayer.py

--- combat_sim_multiplayer.py
+++ combat_sim_multiplayer.py
@@ -3,14 +3,18 @@ import random
 play = True
 crit2 = 0
 crit1 = 0
-
 while play:
     hp1 = random.randint(20,50)
     hp2 = random.randint(20,50)
     name1 = input("\n>player 1 input name\n\n>")
     name2 = input("\n>player 2 input name\n\n>")
     print(f"\n>{name1} hp:{hp1} vs. {name2} hp:{hp2}\n\n")
-    
+
+    foc1 = 0
+    foc2 = 0
+    op1 = 0
+    op2 = 0
+
     while hp1 > 0 and hp2 > 0:
         atk1 = 0
         atk2 = 0
@@ -18,27 +22,39 @@ while play:
         dn2 = 0
         miss1 = 0
         miss2 = 0
+
         order = random.randint(1,2)
 
+        if op1 == "def":
+            m1 -= 0.2
+        elif op1 != "def":
+            m1 = 1
+
+        if op2 == "def":
+            m2 -= 0.2
+        elif op2 != "def":
+            m2 = 1
+
         if order == 1:
-            op1 = input(f"\n>{name1} choose atk or def\n>")
-            while op1 != "atk" and op1 != "def":
+            op1 = input(f"\n>{name1} choose atk, def or foc\n>")
+            while op1 != "atk" and op1 != "def" and op1 != "foc":
                 print(f">{name1} invalid input")
-                op1 = input(f"\n>{name1} choose atk or def\n>")
-            op2 = input(f"\n>{name2} choose atk or def\n>")
-            while op2 != "atk" and op2 != "def":
+                op1 = input(f"\n>{name1} choose atk, def or foc\n>")
+            op2 = input(f"\n>{name2} choose atk, def or foc\n>")
+            while op2 != "atk" and op2 != "def" and op2 != "foc":
                 print(f">{name2} invalid input")
-                op2 = input(f"\n>{name2} choose atk or def\n>")
+                op2 = input(f"\n>{name2} choose atk, def or foc\n>")
         
+
         if order == 2:
-            op2 = input(f"\n>{name2} choose atk or def\n>")
-            while op2 != "atk" and op2 != "def":
-                print(f"> {name2} invalid input")
-                op2 = input(f"\n>{name2} choose atk or def\n>")
-            op1 = input(f"\n>{name1} choose atk or def\n>")
-            while op1 != "atk" and op1 != "def":
+            op2 = input(f"\n>{name2} choose atk, def or foc\n>")
+            while op2 != "atk" and op2 != "def" and op2 != "foc":
+                print(f">{name2} invalid input")
+                op2 = input(f"\n>{name2} choose atk, def or foc\n>")
+            op1 = input(f"\n>{name1} choose atk, def or foc\n>")
+            while op1 != "atk" and op1 != "def" and op1 != "foc":
                 print(f">{name1} invalid input")
-                op1 = input(f"\n>{name1} choose atk or def\n>")
+                op1 = input(f"\n>{name1} choose atk, def or foc\n>")
 
         if op1 == "atk":
             atk1 = random.randint(1,10)
@@ -46,11 +62,14 @@ while play:
             if miss1 != 10:
                 crit1 = random.randint(1,8)
                 if crit1 == 8:
-                    atk1 *= 2     
+                    atk1 = 2*(atk1+foc1)
             if miss1 == 10:
                 atk1 = 0
-        elif op1 == "def":
-            dn1 = random.randint(0,8)
+        if op1 == "def":
+            dn1 = 20
+        if op1 == "foc":
+            foc1 += 1
+            dn1 = random.randint(0,3)
 
         if op2 == "atk":
             atk2 = random.randint(1,10)
@@ -58,28 +77,41 @@ while play:
             if miss2 != 10:
                 crit2 = random.randint(1,8)
                 if crit2 == 8:
-                    atk2 *= 2     
+                    atk2 *= 2*(atk2+foc2)     
             if miss2 == 10:
                 atk2 = 0
         elif op2 == "def":
-            dn2 = random.randint(0,8)
-            
+            dn2 = 20
+        elif op2 == "foc":
+            foc2 += 1
+            dn2 = random.randint(0,3)
+
         if dn1 > atk2:
             dn1 = atk2
         if dn2 > atk1:
             dn2 = atk1
-            
-        if crit2 == 8 and op1 == "def":
-            atk1 = (atk2 / 2)
-            atk2 = 0
+         
+        if op2 == "atk":            
+            if crit2 == 8 and op1 == "def":
+                atk1 = (atk2 / 2)
+                atk2 = 0
         
-        if crit1 == 8 and op2 == "def":
-            atk2 = (atk1 / 2)
-            qtk1 = 0
-            
-        hp2 -= (atk1 - dn2)
-        hp1 -= (atk2 - dn1)
-        
+        if op1 == "atk":
+            if crit1 == 8 and op2 == "def":
+                atk2 = (atk1 / 2)
+                atk1 = 0
+
+        if op1 == "atk" and atk1 != 0:
+            hp2 -= (atk1 + foc1)-(dn2 * m2) 
+        if op2 == "atk" and atk2 != 0:
+            hp1 -= (atk2 + foc2)-(dn1 * m1)
+
+        if op1 == "def":
+            hp1 += random.randint(1,3)
+        if op2 == "def":
+            hp2 += random.randint(1,3)
+
+        print("\n")
         if miss1 == 10:
             print(f">{name1} missed their attack")
         if miss2 == 10:
@@ -88,7 +120,7 @@ while play:
         if op1 == "atk":
             if atk1 != 0:
                 if crit1 != 8:
-                    print(f">{name1} dealt {atk1} damage")
+                    print(f">{name1} dealt {atk1 + foc1} damage")
                 else:
                     if op2 != "def":
                         print(f">{name1} crit for {atk1} damage")
@@ -97,11 +129,13 @@ while play:
         if op1 == "def":
             if atk2 != 0:
                 print(f">{name1} blocked {dn1} damage")
+        if op1 == "foc":
+            print(f">{name1} focused to a level of {foc1}")
 
         if op2 == "atk":
             if atk2 != 0:
                 if crit2 != 8:
-                    print(f">{name2} dealt {atk2} damage")
+                    print(f">{name2} dealt {atk2+foc2} damage")
                 else:
                     if op1 != "def":
                         print(f">{name2} crit for {atk2} damage")
@@ -110,8 +144,11 @@ while play:
         if op2 == "def":
             if atk1 != 0:
                 print(f">{name2} blocked {dn2} damage")
-        
-        print(f">{name1} has {hp1} health remaining\n>{name2} has {hp2} health remaining")        
+        if op2 == "foc":
+            print(f">{name2} focused to a level of {foc2}")
+        if op1 == "def" and op2 == "def":
+            print(f">Moronicly, both {name1} and {name2} tried to defend eachother")
+        print(f"\n>{name1} has {hp1} health remaining\n>{name2} has {hp2} health remaining")        
             
     if hp2 <= 0 and hp1 >= 0:
         print(f">{name1} wins!")
