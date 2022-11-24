6ab5c4e0b1f08656678046e93451ebd45423c26b
Temple <340888700@EDU.YRDSB.NET>
2022-11-24 12:00:59 -0500

    idk

--- idk.py
+++ idk.py
@@ -1,9 +1,3 @@
-n = int(input("give a number\n>"))
-
-def rangeinput(n):
-    if n >= 1 and n <= 5:
-        print("that number is between 1 and 5")
-    else:
-        print("that number is not between 1 and 5")
-
-rangeinput(n)
+def exponent (b, e):
+    for e in 
+    6*6*6*6*6*6*6*6*6
--- raises.py
+++ raises.py
@@ -3,21 +3,3 @@ print(names[1])
 
 for name in names:
     print(name)
-
-for i in range(52, 145, 0.5):
-    print(names [i])
-
-i = 52.5
-
-while i <= 145:
-    print(i)
-    i = i+0.5
-number = 78
-while number >= 10:
-    print("type a number under 10")
-    number = int(input())
-    if number == 10:
-        print("10 is not included")
-
-
-
