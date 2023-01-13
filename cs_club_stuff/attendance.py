bf793e14cd13b7c0f961f510ae6df8571d52cd9e
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-01-13 12:04:21 -0500

    added an end option because daniel didn't

--- cs_club_stuff/attendance.py
+++ cs_club_stuff/attendance.py
@@ -1,12 +1,15 @@
 csclub = {}
+yes = True
 def main():
-    start = int(input("enter 1 to add names, enter 2 to delete names, enter 3 to show the list of naems."))
+    start = int(input("enter 1 to add names, enter 2 to delete names, enter 3 to show the list of names, or enter 4 to end the script."))
     if start == 1:
         add()
     elif start == 2:
         dele()
     if start == 3:
         show()
+    elif start == 4:
+        end()
 def add():
     name = input("Enter in the name of the member you would like to add:")
     grade = int(input("Enter in the grade of the member:"))
@@ -20,6 +23,7 @@ def dele():
         print("This member does not exist in the list")
 def show():
     print(csclub)
-
-while True:
+def end():
+    yes = False
+while yes is True:
     main()
