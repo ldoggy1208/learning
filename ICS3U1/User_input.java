2497bf5a6fce195182eab281eea14f5182894a18
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-09 12:19:14 -0500

    testing the capabilities of my computer

---
+++ ICS3U1/User_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class User_input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt());
+    }
+
+    
+}
