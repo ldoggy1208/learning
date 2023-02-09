0f8c7d1fdd8e090fdd6b5ad7545ea32994be7462
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-09 10:41:28 -0500

    made my favourites assignment

---
+++ ICS3U1/LiamMyFavourites.java
@@ -0,0 +1,45 @@
+/* This is a program used to designate
+ * my favourite item of a few topics
+ * and to answer a few questions about me
+ * 
+ * @Author Temple
+ * @Date Feb 9 2023
+ * @Version 1.0
+ */
+
+public class LiamMyFavourites {
+    public static void main(String[] args) {
+/*1. What is my name
+ *2. What is your favorite color?
+ *3. What is your favorite TV show?
+ *4. What is your favorite movie?
+ *5. What is your favorite computer game?
+ *6. What is your favorite food?
+ *7. What is your favorite Disney character or movie?
+ *8. Who is your favorite singer/group/musician?
+ *9. What is your favorite sport?
+ *10. What is your favorite sports team?
+ *11. What is your favorite book?
+ *12. What is your favorite subject?
+ *13. What city would you like to visit?
+ *14. Do you have any siblings? How many?
+ *15. Why did you take computer programming?      
+*/   
+        System.out.println("My name is Liam Temple.");
+        System.out.println("My favourite colour is purple.");
+        System.out.println("My favourite TV show is Family\tGuy.");
+        System.out.println("I don't have a favourite movie");
+        System.out.println("My favourit tvideo game is \"Dark Souls III\"");
+        // I assume by computer game video game was meant
+        System.out.println("My favourite food is probably pizza");
+        System.out.println("My favourite disney movie is \"The Lion King\"");
+        System.out.println("My favourite musical group is The Longest Johns");
+        System.out.println("My favourite sport is baseball");
+        System.out.println("My favourite sports team is the Toronto Blue Jays");
+        System.out.println("My favourite book is \"Warriors: into the forest\" by E.Hunter");
+        System.out.print("I have 2 siblings\nAn older brother named Ethan\nA younger sister named Olivia");
+        System.out.println("I chose to take computer programming because I recently got into coding with python and I assumed I should enhance my coding knowledge");
+
+    }// end of main method
+
+}// end of class
