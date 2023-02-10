6e3c6b8c9755a1c199e297f6cf7c30c6149931b0
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-10 09:20:04 -0500

    random

--- ICS3U1/Formating.java
+++ ICS3U1/Formating.java
@@ -1,5 +1,8 @@
+import java.sql.Date;
+import java.util.Locale;
+
 public class Formating {
     public static void main(String[] args) {
-        
+        System.out.printf("%tT%n", date);       
     }
 }
