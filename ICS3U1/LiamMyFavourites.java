ef6e876089d18fa270a8248a45402d413e9d6bbb
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-10 08:27:10 -0500

    numbered the questions and added missing questions

--- ICS3U1/LiamMyFavourites.java
+++ ICS3U1/LiamMyFavourites.java
@@ -9,7 +9,8 @@
 
 public class LiamMyFavourites {
     public static void main(String[] args) {
-/*1. What is my name
+/* Here is a list of the questions that will be answered
+ *1. What is my name
  *2. What is your favorite color?
  *3. What is your favorite TV show?
  *4. What is your favorite movie?
@@ -25,20 +26,22 @@ public class LiamMyFavourites {
  *14. Do you have any siblings? How many?
  *15. Why did you take computer programming?      
 */   
-        System.out.println("My name is Liam Temple.");
-        System.out.println("My favourite colour is purple.");
-        System.out.println("My favourite TV show is Family\tGuy.");
-        System.out.println("I don't have a favourite movie");
-        System.out.println("My favourit tvideo game is either \"Dark Souls III\"\t or\t \"Hollow Knight\"");
+        System.out.println("1:\tMy name is Liam Temple.");
+        System.out.println("2:\tMy favourite colour is purple.");
+        System.out.println("3:\tMy favourite TV show is Family Guy.");
+        System.out.println("4:\tI don't have a favourite movie.");
+        System.out.println("5:\tMy favourite video game is either \"Dark Souls III\"\t or\t \"Hollow Knight\".");
         // I assume by computer game video game was meant
-        System.out.println("My favourite food is probably pizza");
-        System.out.println("My favourite disney movie is \"The Lion King\"");
-        System.out.println("My favourite musical group is The Longest Johns");
-        System.out.println("My favourite sport is baseball");
-        System.out.println("My favourite sports team is the Toronto Blue Jays");
-        System.out.println("My favourite book is \"Warriors: into the forest\" by E.Hunter");
-        System.out.print("I have 2 siblings\nAn older brother named Ethan\nA younger sister named Olivia");
-        System.out.println("I chose to take computer programming because I recently got into coding with python and I assumed I should enhance my coding knowledge");
+        System.out.println("6:\tMy favourite food is probably pizza.");
+        System.out.println("7:\tI don't have a particularly favourite Disney movie or character.");
+        System.out.println("8:\tMy favourite musical group is The Longest Johns.");
+        System.out.println("9:\tMy favourite sport is baseball.");
+        System.out.println("10:\tMy favourite sports team is the Toronto Blue Jays.");
+        System.out.println("11:\tMy favourite book is \"Warriors: into the forest\" by E.Hunter.");
+        System.out.println("12:\tMy favourite school subject is math.")
+        System.out.println("13:\tIf I had to choose a country to visit, I would choose Yellowknife.");
+        System.out.print("14:\tI have 2 siblings.\nAn older brother named Ethan.\nA younger sister named Olivia.\n");
+        System.out.println("15:\tI chose to take computer programming because I recently got into coding with python and I assumed I should enhance my coding knowledge.");
 
     }// end of main method
 
--- ICS3U1/User_input.java
+++ ICS3U1/User_input.java
@@ -1,10 +1,1 @@
-import java.util.Scanner;
 
-public class User_input {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println(in.nextInt());
-    }
-
-    
-}
