167d1eb3030c1f24d85ac912f3cadbe38e58a449
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-15 08:28:02 -0500

    Update TempleVariables.java

--- ICS3U1/TempleVariables.java
+++ ICS3U1/TempleVariables.java
@@ -12,7 +12,7 @@ public class TempleVariables {
         //This is the creation of the variables
 
         System.out.print("The length of the rectangle is "+length);
-        System.out.print(" cm long\n The width of the rectangle is "+width);
+        System.out.print(" cm long\nThe width of the rectangle is "+width);
         System.out.print(" cm wide");
         //This is the displaying of the length and width
 
@@ -26,7 +26,7 @@ public class TempleVariables {
         //end of rectangle question
 
         double radius = 14;
-        //settingg of the radius
+        //setting of the radius
 
         System.out.print("\n\nThe radius of the circle is "+radius);
         System.out.print(" cm");
@@ -35,7 +35,7 @@ public class TempleVariables {
         double circumference = 2*3.14*radius;
         //calculating the circumference and area of the circle
         System.out.print("\nThe area of the circle is "+area2);
-        System.out.print("cm^2");
+        System.out.print(" cm^2");
         System.out.print("\nThe circumference of the circle is "+circumference);
         System.out.print(" cm");
         //declaring the circumference and area of a circle
@@ -51,5 +51,18 @@ public class TempleVariables {
         System.out.print(" degrees fahrenheit");
         //declaring the temperatures
         //end of temperature question
+
+        double Physics = 76.7;
+        double Entrepreneurship = 77.0;
+        double Foods = 83.8;
+        double Math = 90.4;
+        double Average = 0;
+        //setting the variables
+        Average = (Physics + Math + Foods + Entrepreneurship)/4;
+        //calculating the average
+
+        System.out.print("\n\nThe average of all my marks was "+Average);
+        //declaring the average
+        //end of marks question
     }//end of main method
 }//end of class
