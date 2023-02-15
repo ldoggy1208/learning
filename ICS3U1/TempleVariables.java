c3302de9dc6f91b4adaaeaa2261ff844d3827a1a
ldoggy1208 <75604231+ldoggy1208@users.noreply.github.com>
2023-02-15 13:13:32 -0500

    made record keeping mostly for english

--- ICS3U1/TempleVariables.java
+++ ICS3U1/TempleVariables.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /* This file is for displaying my knowledge
  * of different types of variables and how
  * to calculate simple equations
@@ -6,15 +7,18 @@ public class TempleVariables {
 
     public static void main(String[] args) {
 
-        int width = 10;
-        int length = 20;
+        int width;
+        int length;
 
         //This is the creation of the variables
-
-        System.out.print("The length of the rectangle is "+length);
-        System.out.print(" cm long\nThe width of the rectangle is "+width);
-        System.out.print(" cm wide");
-        //This is the displaying of the length and width
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input the length of the rectangle\n>");
+        length = scan.nextInt();
+        System.out.print("The length of the rectangle is "+length +"cm long\n");
+        System.out.print("Input the width of the rectangle\n>");
+        width = scan.nextInt();
+        System.out.print("\nThe width of the rectangle is "+width +"cm wide\n");
+        //This is the displaying and getting of the length and width
 
         int area = length * width;
         int perimeter = 2 * length + 2 * width;
@@ -25,30 +29,29 @@ public class TempleVariables {
         System.out.print(" cm^2");
         //end of rectangle question
 
-        double radius = 14;
+        double radius;
         //setting of the radius
-
-        System.out.print("\n\nThe radius of the circle is "+radius);
-        System.out.print(" cm");
-        //declaring the radius
+        
+        System.out.print("\n\nInput the radius of the circle\n>");
+        radius = scan.nextInt();
+        System.out.print("The radius of the circle is "+radius +"cm");
+        //declaring and setting the radius
         double area2 = radius * radius * 3.14;
         double circumference = 2*3.14*radius;
         //calculating the circumference and area of the circle
-        System.out.print("\nThe area of the circle is "+area2);
-        System.out.print(" cm^2");
-        System.out.print("\nThe circumference of the circle is "+circumference);
-        System.out.print(" cm");
+        System.out.print("\nThe area of the circle is "+area2 +"cm^2");
+        System.out.print("\nThe circumference of the circle is "+circumference +"cm");
         //declaring the circumference and area of a circle
         //end of circle question
 
         
-        int tempC = 100;
-        int tempF = 0;
-
+        int tempC;
+        int tempF;
+        System.out.print("\n\nInput the temperature in Celsius\n>");
+        tempC = scan.nextInt();
         tempF = tempC * 9 / 5 + 32;
         //setting and calculating the temperatures
-        System.out.print("\n\n100 degrees celsius is equal to "+tempF);
-        System.out.print(" degrees fahrenheit");
+        System.out.print(tempC +" degrees celsius is equal to "+tempF +" Degrees Fahrenheit");
         //declaring the temperatures
         //end of temperature question
 
